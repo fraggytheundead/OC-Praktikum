b45b083a1e48ea020c31e3a0ba823c2fbdc90aa0
Stephan Lohse <lohse@informatik.uni-luebeck.de>
2009-11-11 23:02:04 +0100

    renamed things, added comments and changed the way the deadzones of the joystick are done (via defines now, rather than hardcoded)

--- roomba_acc_remote/sender/src/RoombaAccRemote.cpp
+++ roomba_acc_remote/sender/src/RoombaAccRemote.cpp
@@ -44,8 +44,25 @@
 #define TASK_START_COMMUNICATION				4
 #define TASK_CONNECT_BLINK						5
 
+// Fine tuning for the Joystick:
+// Dead zones around the center and the maximum displacement
+#define ACCEL_DEADZONE_CENTER_XAXIS				200
+#define ACCEL_DEADZONE_CENTER_YAXIS				200
+#define ACCEL_DEADZONE_FULL_XAXIS				200
+#define ACCEL_DEADZONE_FULL_YAXIS				200
 
-//#define EM
+#define ACCEL_FULL_VALUE_XAXIS					1000 - (ACCEL_DEADZONE_CENTER_XAXIS + ACCEL_DEADZONE_FULL_XAXIS)
+#define ACCEL_FULL_VALUE_YAXIS					1000 - (ACCEL_DEADZONE_CENTER_YAXIS + ACCEL_DEADZONE_FULL_YAXIS)
+
+// Limiting the Roombas Speed:
+#define ROOMBA_MAX_SPEED						500
+#define ROOMBA_TURN_SPEED						250
+
+
+//#define DEBUG_JOYSTICK
+//#define DEBUG_JOYSTICK_VERBOSE
+
+
 #define SM
 //----------------------------------------------------------------------------
 /**
@@ -53,7 +70,7 @@
 
 using namespace isense;
 
-class SmartHomeApp :
+class RoombaAccRemote :
 	public isense::Application
 	,public isense::Receiver
 	,public isense::Sender
@@ -61,25 +78,18 @@ class SmartHomeApp :
 	,public isense::TimeoutHandler
 	,public isense::SleepHandler
 	,public ButtonHandler
-#ifdef EM
-	,public Int8DataHandler
-	,public Uint32DataHandler
-#endif
 #ifdef SM
 	,public BufferDataHandler
 	,public SensorHandler2
 #endif
 {
 public:
-	SmartHomeApp(isense::Os& os);
+	RoombaAccRemote(isense::Os& os);
 
-	virtual ~SmartHomeApp() ;
+	virtual ~RoombaAccRemote() ;
 
 	///From isense::Application
 	virtual void boot (void) ;
-
-	// boots the EnvironmentModule
-	void boot_EM();
 
 	//boots the SecurityModule
 	void boot_SM();
@@ -117,15 +127,6 @@ public:
 	///From isense::TimeoutHandler
 	virtual void timeout( void* userdata ) ;
 
-#ifdef EM
-	// inherited from Int8DataHandler, called by the temperature
-	// sensor if the temperature threshold is exceeded
-	void handle_int8_data( int8 value );
-
-	// inherited from UInt32DataHandler, called by the light
-	// sensor upon luminance changes
-	void handle_uint32_data( uint32 value );
-#endif
 #ifdef SM
 	// inherited from BufferDataHandler, called when
 	// accelerometer data is available
@@ -139,10 +140,6 @@ public:
 
 private:
 	CoreModule *cm_;
-#ifdef EM
-	// pointer to the Environmental Board
-	EnvironmentModule* em_;
-#endif
 #ifdef SM
 	// pointer to the accelerometer
 	LisAccelerometer* acc_;
@@ -156,29 +153,16 @@ private:
 };
 
 //----------------------------------------------------------------------------
-SmartHomeApp::
-	SmartHomeApp(isense::Os& os)
+RoombaAccRemote::
+	RoombaAccRemote(isense::Os& os)
 	: isense::Application(os)
 	,cm_(new CoreModule(os))
 	{
-#ifdef EM
-	// create EnvironmentModule instance
-	em_ = new EnvironmentModule(os);
-#endif
 #ifdef SM
 	// create LisAccelerometer instance
 	acc_ = new LisAccelerometer(os);
 	// create PriSensor2 instance
 	pir_ = new PirSensor2(os);
-#endif
-
-    boot_EM();
-#ifdef EM
-        if (em_->temp_sensor() != NULL)
-			{
-				// Der Temperatursensor sagt bescheid, wenn es w�rmer als 31 Grad oder k�lter als 29 Grad wird.
-				em_->temp_sensor()->set_threshold(31,29);
-            }
 #endif
 
     boot_SM();
@@ -192,15 +176,15 @@ SmartHomeApp::
 	}
 
 //----------------------------------------------------------------------------
-SmartHomeApp::
-	~SmartHomeApp()
+RoombaAccRemote::
+	~RoombaAccRemote()
 
 	{
 	}
 
 //----------------------------------------------------------------------------
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	boot(void)
 	{
         // Die Applikation startet
@@ -220,7 +204,7 @@ void
  	}
 
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	button_down( uint8 button )
 	{
         // Einer der beiden Kn�pfe wurde gedr�ckt
@@ -228,7 +212,7 @@ void
 	}
 //----------------------------------------------------------------------------
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	execute( void* userdata )
 	{
 	switch((int) userdata) {
@@ -256,6 +240,7 @@ void
 			os_.add_timeout_in(Time(3000), this, (uint8*) TASK_PINGPONG);
 			break;
 		case TASK_START_COMMUNICATION:
+			// send out requests to connect to a roomba
 			if(starting_communication) {
 				uint8 buf[] = {ROOMBA_REMOTE_START_COMMUNICATION};
 				os_.radio().send(0xffff, 1, buf, 0, NULL);
@@ -263,6 +248,7 @@ void
 			}
 			break;
 		case TASK_CONNECT_BLINK:
+			// blink to indicate, that we are currently not connected to a roomba
 			if(connectedTo) {
 				connectLed = true;
 				cm_->led_on();
@@ -281,120 +267,126 @@ void
 			//nothing
 			break;
 		}
-
-#ifdef EM
-		if ( (uint32)userdata == TASK_SET_LIGHT_THRESHOLD)
-		{
-			// Der Lichtsensor sagt bescheid wenn sich die Helligkeit um 60% ge�ndert hat
-			em_->light_sensor()->enable_threshold_interrupt(true, 60);
-		}
-#endif
 	}
 
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	timeout( void* userdata )
 	{
 		os_.add_task( this, userdata);
 	}
 
-//--------------------------------------------------------------
-#ifdef EM
-void
-	SmartHomeApp::
-	handle_uint32_data( uint32 value )
-	{
-		// Der Lichtsensor hat eine �nderung der Helligkeit festgestellt.
-	}
-#endif
-
-#ifdef EM
-void
-	SmartHomeApp::
-	handle_int8_data( int8 value )
-	{
-		// Der Temperatursensor hat festgestellt, dass der Schwellwert �ber oder unterschitten wurde
-	}
-#endif
-//--------------------------------------------------------------
 #ifdef SM
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	handle_buffer_data( BufferData* buf_data )
 	{
 		// Der Beschleunigungssensor hat eine Bewegung wahrgenommen
 		acc_->set_mode(MODE_THRESHOLD);
 		int16 xAchse = buf_data->buf[0];
 		int16 yAchse = buf_data->buf[1];
-//		int16 zAchse = buf_data->buf[2];
-//		os_.debug("x: %i, y: %i", xAchse, yAchse);
+#ifdef DEBUG_JOYSTICK
+		os_.debug("x: %i, y: %i", xAchse, yAchse);
+#endif
 		int16 speed_factor = 0;
 		int16 radius_factor = 0;
 		int16 speed;
 		int16 radius;
-		if(xAchse < 200 && xAchse > -200) {
-//			os_.debug("speed_factor = 0");
+		if(xAchse < ACCEL_DEADZONE_CENTER_XAXIS && xAchse > -ACCEL_DEADZONE_CENTER_XAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+			os_.debug("speed_factor = 0");
+#endif
 			speed_factor = 0;
-		} else if(xAchse >= 200) {
-//			os_.debug("xAchse >= 200");
-			speed_factor = (xAchse - 200);
-			if(speed_factor > 600) {
-//				os_.debug("xAchse > 600");
+		} else if(xAchse >= ACCEL_DEADZONE_CENTER_XAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+			os_.debug("xAchse >= 200");
+#endif
+			speed_factor = (xAchse - ACCEL_DEADZONE_CENTER_XAXIS);
+			if(speed_factor > ACCEL_FULL_VALUE_XAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("xAchse > 600");
+#endif
 				speed_factor = 100;
 			} else {
-				speed_factor = (speed_factor * 100 / 600);
-//				os_.debug("xAchse <= 600, speed_factor: %i", speed_factor);
+				speed_factor = (speed_factor * 100 / ACCEL_FULL_VALUE_XAXIS);
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("xAchse <= 600, speed_factor: %i", speed_factor);
+#endif
 			}
 		} else {
-//			os_.debug("xAchse <= -200");
-			speed_factor = (xAchse + 200);
-			if(speed_factor < -600) {
-//				os_.debug("xAchse < -600, speed_factor = -100");
+#ifdef DEBUG_JOYSTICK_VERBOSE
+			os_.debug("xAchse <= -200");
+#endif
+			speed_factor = (xAchse + ACCEL_DEADZONE_CENTER_XAXIS);
+			if(speed_factor < -ACCEL_FULL_VALUE_XAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("xAchse < -600, speed_factor = -100");
+#endif
+
 				speed_factor = -100;
 			} else {
-				speed_factor = (speed_factor * 100 / 600);
-//				os_.debug("xAchse <= -600, speed_factor: %i", speed_factor);
+				speed_factor = (speed_factor * 100 / ACCEL_FULL_VALUE_XAXIS);
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("xAchse <= -600, speed_factor: %i", speed_factor);
+#endif
 			}
 		}
 
-		if(yAchse < 200 && yAchse > -200) {
-//			os_.debug("radius = 0");
+		if(yAchse < ACCEL_DEADZONE_CENTER_YAXIS && yAchse > -ACCEL_DEADZONE_CENTER_YAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+			os_.debug("radius = 0");
+#endif
 			radius_factor = 0;
-		} else if(yAchse >= 200) {
-//			os_.debug("yAchse >= 200");
-			radius_factor = (yAchse - 200);
-			if(radius_factor > 600) {
-//				os_.debug("yAchse > 600");
+		} else if(yAchse >= ACCEL_DEADZONE_CENTER_YAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+			os_.debug("yAchse >= 200");
+#endif
+			radius_factor = (yAchse - ACCEL_DEADZONE_CENTER_YAXIS);
+			if(radius_factor > ACCEL_FULL_VALUE_YAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("yAchse > 600");
+#endif
 				radius_factor = 100;
 			} else {
-				radius_factor = (radius_factor * 100 / 600);
-//				os_.debug("yAchse <= 600, radius_factor: %i", radius_factor);
+				radius_factor = (radius_factor * 100 / ACCEL_FULL_VALUE_YAXIS);
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("yAchse <= 600, radius_factor: %i", radius_factor);
+#endif
 			}
 		} else {
-//			os_.debug("yAchse <= -200");
-			radius_factor = (yAchse + 200);
-			if(radius_factor < -600) {
-//				os_.debug("yAchse < -600, radius_factor = -100");
+#ifdef DEBUG_JOYSTICK_VERBOSE
+			os_.debug("yAchse <= -200");
+#endif
+			radius_factor = (yAchse + ACCEL_DEADZONE_CENTER_YAXIS);
+			if(radius_factor < -ACCEL_FULL_VALUE_YAXIS) {
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("yAchse < -600, radius_factor = -100");
+#endif
 				radius_factor = -100;
 			} else {
-				radius_factor = (radius_factor * 100 / 600);
-//				os_.debug("yAchse <= -600, radius_factor: %i", radius_factor);
+				radius_factor = (radius_factor * 100 / ACCEL_FULL_VALUE_YAXIS);
+#ifdef DEBUG_JOYSTICK_VERBOSE
+				os_.debug("yAchse <= -600, radius_factor: %i", radius_factor);
+#endif
 			}
 		}
 
-//		os_.debug("speed_factor: %i, radius_factor: %i", speed_factor, radius_factor);
+#ifdef DEBUG_JOYSTICK
+		os_.debug("speed_factor: %i, radius_factor: %i", speed_factor, radius_factor);
+#endif
 
-		speed = (speed_factor * 500) / 100;
+		speed = (speed_factor * ROOMBA_MAX_SPEED) / 100;
 
 		if(radius_factor==0) {
 			radius = 32768;
 		} else if (radius_factor == 100){
-			// auf der Stelle drehen
+			// turn on the spot
 			radius = 0;
-			speed = 250;
+			speed = ROOMBA_TURN_SPEED;
 		} else if (radius_factor == -100) {
+			// turn on the spot in the other direction
 			radius = 0;
-			speed = -250;
+			speed = -ROOMBA_TURN_SPEED;
 		} else if (radius_factor > 0){
 			radius = 2000 - ((radius_factor * 2000) / 100);
 		} else {
@@ -407,7 +399,7 @@ void
 
 #ifdef SM
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	handle_sensor(bool this_is_the_end, uint16 interrupt_count)
 	{
 		// Der PIR Sensor hat eine oder mehrere Bewegungen wahrgenommen
@@ -417,10 +409,10 @@ void
 #endif
 //----------------------------------------------------------------------------
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	receive (uint8 len, const uint8 * buf, uint16 src_addr, uint16 dest_addr, uint16 lqi, uint8 seq_no, uint8 interface)
 	{
-        // Der Sensorknoten hat eine Nachricht empfangen
+        // a roomba has answered our requests, Connection established
 		if(!connectedTo && dest_addr == os_.id()) {
 			if(len > 0 && buf[0] == ROOMBA_REMOTE_PING ) {
 				uint8 buffer[] = {ROOMBA_REMOTE_PONG};
@@ -431,18 +423,21 @@ void
 				lastContact = os_.time();
 			}
 		}
+		// we have been PINGed by our roomba, PONG back!
 		if(connectedTo == src_addr && len > 0 && buf[0] == ROOMBA_REMOTE_PING) {
 			uint8 buffer[] = {ROOMBA_REMOTE_PONG};
 			os_.radio().send(src_addr, 1, buffer, 0, NULL);
 			lastContact = os_.time();
 		}
+		// our roomba has answered our PING with a PONG, remember this as the last time
+		// we had contact to our roomba
 		if(connectedTo == src_addr && len > 0 && buf[0] == ROOMBA_REMOTE_PONG) {
 			lastContact = os_.time();
 		}
 	}
 
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	send(bool temp, bool light, bool acc, bool pir, bool on)
 	{
         // Benutze diese Methode um eine Nachricht zu versenden
@@ -450,61 +445,14 @@ void
 
 //----------------------------------------------------------------------------
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	confirm (uint8 state, uint8 tries, isense::Time time)
 	{
 	}
 
-//----------------------------------------------------------------------------
 
 void
-	SmartHomeApp::
-	boot_EM()
-	{
-#ifdef EM
-		// if allocation of EnvironmentModule was successful
-		if (em_!=NULL)
-		{
-			if (em_->light_sensor() != NULL)
-			{
-				//--------- configure the light sensor ----------
-				// set this application as the data handler called upon
-				// luminance changes beyond the set percentage
-				em_->light_sensor()->set_data_handler(this);
-				// add a task to be called in a second. Then, the
-				// threshold mode is set one second after boot, to give the
-				// sensor time for its first conversion
-				os().add_task_in(Time(1,0), this, (void*)TASK_SET_LIGHT_THRESHOLD);
-			} else
-				os().fatal("Could not allocate light sensor");
-
-
-			if (em_->temp_sensor() != NULL)
-			{
-				//--------- configure the light sensor ----------
-				// set the temperature threshold to 35�C and the hysteresis
-				// value to 30�C
-				em_->temp_sensor()->set_threshold(31,29);
-				// set this application as the data handler called if
-				// temperature threshold is exceed or falls back below
-				// the hysteresis value
-				em_->temp_sensor()->set_data_handler(this);
-			} else
-				os().fatal("Could not allocate temp sensor");
-
-			// enable the Environmental Sensor Module,
-			// including its sensors
-			em_->enable(true);
-
-			// register task to be called in a minute for periodic sensor readings
-			//os().add_task_in(Time(60,0), this, (void*)TASK_READ_SENSORS);
-		} else
-			os().fatal("Could not allocate EnvironmentModule");
-#endif
-	}
-
-void
-	SmartHomeApp::
+	RoombaAccRemote::
 	boot_SM()
 	{
 #ifdef SM
@@ -541,7 +489,7 @@ void
 
 //----------------------------------------------------------------------------
 bool
-	SmartHomeApp::
+	RoombaAccRemote::
 	stand_by (void)
 	{
 		os_.debug("App::sleep");
@@ -550,7 +498,7 @@ bool
 
 //----------------------------------------------------------------------------
 bool
-	SmartHomeApp::
+	RoombaAccRemote::
 	hibernate (void)
 	{
 		os_.debug("App::hibernate");
@@ -559,7 +507,7 @@ bool
 
 //----------------------------------------------------------------------------
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	wake_up (bool memory_held)
 	{
 		os_.debug("App::Wakeup");
@@ -570,11 +518,11 @@ void
   */
 isense::Application* application_factory(isense::Os& os)
 {
-	return new SmartHomeApp(os);
+	return new RoombaAccRemote(os);
 }
 
 void
-	SmartHomeApp::
+	RoombaAccRemote::
 	drive (int16 speed, int16 radius)
 	{
 		os_.debug("Speed: %i, Radius: %i", speed, radius);
